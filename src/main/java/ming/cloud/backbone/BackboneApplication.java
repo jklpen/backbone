33e6ccaa6a267f2e59d6c022b5435be254d31909
Ming <ming3109@outlook.com>
2018-12-25 21:55:15 +0800

    初始化

---
+++ src/main/java/ming/cloud/backbone/BackboneApplication.java
@@ -0,0 +1,14 @@
+package ming.cloud.backbone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class BackboneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackboneApplication.class, args);
+    }
+}
